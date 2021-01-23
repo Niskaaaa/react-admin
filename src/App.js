cb542ca6ada44e0f2aca8c013005198a5388a5e7
Niskaaaa <1162314353@qq.com>
2021-01-23 23:23:27 +0800

    form style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import { Button,message } from 'antd'
+import {BrowserRouter,Route, Switch} from'react-router-dom'
+import Login from'./pages/login/login'
+import Admin from'./pages/admin/admin'
+export default class App extends Component{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  alert=()=>{
+    message.success('success')
+  }
+  render(){
+    return (
+      <BrowserRouter>
+      <Switch>{/*只匹配其中一个 */}
+      <Route path='/login' component={Login}>
 
-export default App;
+      </Route>
+      <Route path='/' component={Admin}>
+
+      </Route>
+</Switch>
+      </BrowserRouter>
+    )
+  }
+};
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import "./login.less";
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+class Login extends React.Component {
+    
+
+    onFinish = (values) => {
+      console.log('Received values of form: ', values);
+    };
+ 
+
+  render() {
+    return (
+      <div className="login">
+        <header className="login-header">
+          <h1>后台管理系统</h1>
+        </header>
+        <section className="login-content">
+          <h2>用户登录</h2>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
+            onFinish={this.onFinish}
+          >
+            <Form.Item
+              name="username"
+              rules={[
+                { required: true, message: "Please input your Username!" },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Username"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+
+              <a className="login-form-forgot" href="">
+                Forgot password
+              </a>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+              >
+                Log in
+              </Button>
+              Or <a href="">register now!</a>
+            </Form.Item>
+          </Form>
+        </section>
+      </div>
+    );
+  }
+}
+
+
+
+export default Login;
